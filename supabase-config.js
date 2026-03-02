81a42f71c5152e464baa6b5569845e5491264837
Bripa75 <ripabrian93@gmail.com>
2026-03-01 19:30:08 -0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,2 @@
-// supabase-config.js
-// Paste these from Supabase Dashboard -> Project Settings -> API
-// (It's OK that the anon key is in the frontend; it is meant to be public.)
-
 export const SUPABASE_URL = "https://qfsdfitezntantlrvkkl.supabase.co";
-export const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+export const SUPABASE_ANON_KEY = "sb_publishable_agDVr7Rw2cObFvxrWjNd1g_VvKhQ8AB";
